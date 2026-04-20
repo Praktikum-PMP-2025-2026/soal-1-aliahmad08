ad9c490a122898d3cd41eebee7339f62f6fc2c5f
aliahmad08 <aliahmadizzawahyudi@gmail.com>
2026-04-20 10:21:01 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -9,6 +9,7 @@
  *
  * 
  */
+
 #include <stdio.h>
 #include <math.h>
 
@@ -50,7 +51,7 @@ int main() {
 
             
             if (cekkiri && cekkanan) {
-                arr[i] = (int)floor((kiri + kanan) / 2.0); 
+                arr[i] = (int) floor ((kiri + kanan) / 2); 
             } else if (cekkiri) {
                 arr[i] = kiri;
             } else if (cekkanan) {
